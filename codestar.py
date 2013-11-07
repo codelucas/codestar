a1b65bdbc12c8fd6eb445d60c60648fc76b81a61
Jason <jasontanner328@gmail.com>
2013-11-07 18:44:58 -0500

    began work on template skeletons and routing

--- codestar.py
+++ codestar.py
@@ -7,3 +7,23 @@ __author__ = 'Lucas Ou-Yang, Jason Tanner'
 __date__ = 'November 2nd, 2013'
 __version__ = '0.0.1'
 
+# Imports
+from flask import Flask
+from flask import render_template
+
+app = Flask(__name__)
+
+# routing and controllers
+# =======================
+
+@app.route('/')
+def home():
+    return render_template('home.html')
+
+@app.route('/about')
+def about():
+    return render_template('about.html')
+
+if __name__ == '__main__':
+    app.run(debug=True)
+
